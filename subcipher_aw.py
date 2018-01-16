bac0375f398e451e273fa346c35f0b100fa7af5f
Quirkster <anna@weiksner.com>
2018-01-16 08:33:16 -0500

    added changes

--- subcipher_aw.py
+++ subcipher_aw.py
@@ -1,8 +1,9 @@
 import pyperclip, sys, random
 LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 def main():
-    myMessage = 'If a man is offred a fact which goes against his instincts, he will scrutinize it closely, unless the evidence is overwhelming, he will refuse to believe it. If, on the other hand, he is offered something which affords a reason for acting in accordance to his instincts, he will accept it even on the slightest evidence. The origin of myths is explained this way. Betrand Russel'
-    myKey ='LWOAYUISVKMNXPBDCRJOEGHZ'
+    myMessage = 'If a man is offered a fact which goes against his instincts, he will scrutinize it closely, unless the evidence is overwhelming, he will refuse to believe it. If, on the other hand, he is offered something which affords a reason for acting in accordance to his instincts, he will accept it even on the slightest evidence. The origin of myths is explained this way. Betrand Russel'
+    myKey ='LWOAYUISVKMNXPBDCRJTQEGHZ'#62 then error e-x
+    5 - 23 
     myMode = 'encrypt'
     if keyIsValid(myKey):
         sys.exit('There is an error in the key or symbol set.')
@@ -11,7 +12,7 @@ def main():
     elif myMode == 'decrypt':
         translated = decryptMessage(myKey, myMessage)
     print('Using key %s' % (myKey))
-    print('The %sed message is: '% (myMode))
+    print('The %sed message is:' % (myMode))
     print(translated)
     pyperclip.copy(translated)
     print()
@@ -23,26 +24,32 @@ def keyIsValid(key):
     lettersList.sort()
     return keyList == lettersList
 def encryptMessage(key,message):
-    return translatedMessage(key,message,'encrypt')
-def translatedMessage(key,message,mode):
+    return translateMessage(key,message,'encrypt')
+def decryptMessage(key,message):
+    return translateMessage(key,message, 'decrypt')
+def translateMessage(key,message,mode):
     translated = ''
     charsA = LETTERS
     charsB = key
     if mode == 'decrypt':
         charsA, charsB = charsB, charsA
     for symbol in message:
-        if symbol.upper in charsA:
+        if symbol.upper() in charsA:
             symIndex = charsA.find(symbol.upper())
             if symbol.isupper():
+                #print(charsB)
                 translated += charsB[symIndex].upper()
+                print(translated)
             else:
+                #print(charsB)
                 translated += charsB[symIndex].lower()
+                print(translated)
         else:
             translated += symbol
     return translated
 def getRandomKey():
     key = list(LETTERS)
     random.shuffle(key)
-    return''.join(key)
+    return ''.join(key)
 if __name__ == '__main__':
     main()
